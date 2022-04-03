c2c7ffc9746fc300800d1b0275658492971c100e
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-04-03 10:39:53 +0530

    08-90 adding user component

--- src/components/Users/AddUse.jsx
+++ src/components/Users/AddUse.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 const AddUser = (props) => {
+  const addUserHandler = (e) => {
+    e.preventDefault();
+  };
+
+
   return (
-    <form>
-      <label>UserName</label>
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">UserName</label>
+      <input id="username" type="text"></input>
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" type="number"></input>
+      <button type="submit">Add User</button>
     </form>
   )
 }
